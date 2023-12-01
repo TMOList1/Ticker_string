Corrected Formula for Running text

dj/mytestsite/mytestsite/script/Four.py
@@ -13,22 +13,23 @@ def create_video(text):
 	# Количество кадров в видео
 	num_frames = fps * duration
 
+	# Параметры шрифта и получение ширины строки в пикселях
+	font = cv2.FONT_HERSHEY_COMPLEX 	# Устанавливаем шрифт
+	font_scale = 1
+	font_thickness = 1
+	color = (0,0,0) # цвет текста
+	text_width, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+	wid = text_width[0]
 	for frame_num in range(num_frames):
 		
 		# Инициализируем OpenCV-изображение
 		img = np.zeros((height, width, 3), dtype=np.uint8)
 		img.fill(255)  #Фоновый цвет 0 - чёрный, 255 - белый, кодировка RGB256
-
+		
 		# Перерасчёт координат
-		x = (width - len(text)) - (frame_num * len(text) / 4) * width / (fps * duration)
+		x = (width/2 - wid) * frame_num / (fps * duration)
 		y = height // 2
-
-
 		# Рисуем текст на изображении
-		font = cv2.FONT_HERSHEY_COMPLEX 	# Устанавливаем шрифт
-		font_scale = 1
-		font_thickness = 1
-		color = (0,0,0) # цвет текста
 		cv2.putText(img, text, (int(x), y), font, font_scale, color, font_thickness, cv2.LINE_AA)
 
 		# Добавляем текущий кадр в видео